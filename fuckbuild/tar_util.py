cd483e6a7d89d5df3c9a93c625056547a61927a5
curoky <cccuroky@gmail.com>
2019-05-25 19:51:35 +0800

    Add tar util

---
+++ fuckbuild/tar_util.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author      : IceCory(icecory520@gmail.com)
+# @Copyright(C): GPL 3.0
+# @desc        :
+
+
+def get_fit_cmd(name):
+    if name.endswith("tar.xz"):
+        return "xvf"
+    if name.endswith("tar.gz"):
+        return "zxvf"
