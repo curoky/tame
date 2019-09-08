e116a66b114c02eb334d3c21e57b6aa091ec5f05
curoky <cccuroky@gmail.com>
2019-09-08 23:53:44 +0800

    串联三个阶段，向外提供接口

--- src/__init__.py
+++ src/__init__.py
@@ -3,15 +3,3 @@
 # @Author      : IceCory(icecory520@gmail.com)
 # @Copyright(C): GPL 3.0
 # @desc        :
-
-
-import importlib
-
-
-def get_repo(name):
-    try:
-        repo = importlib.import_module("fuckbuild.package.%s" % name)
-    except ImportError:
-        name = "lib" + name
-        repo = importlib.import_module("fuckbuild.package.%s" % name)
-    return getattr(repo, name)
---
+++ src/goblin.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author      : IceCory(icecory520@gmail.com)
+# @Copyright(C): GPL 3.0
+# @desc        : manage mirror file
+
+
+import logging
+import os
+
+from .builder import Builder, TargetInfo
+from .depender import Depender
+from .downloader import Downloader, global_info
+
+
+class Goblin(object):
+    """
+    主要负责全局的资源管理
+    """
+
+    def __init__(self, root, name, versions, install_path,
+                 build_thread_num, download_thread_num):
+        self.name = name
+        self.root = root
+        self.logger = logging.getLogger(__name__)
+
+        self.builder = Builder(root, build_thread_num)
+        self.depender = Depender()
+        self.downloader = Downloader(root, download_thread_num)
+
+        self.install_path = install_path or os.path.join(self.root, "install")
+        if not os.path.exists(self.root):
+            os.makedirs(self.root)
+
+        self.targets_list = []
+        self.targets_map = {}
+        self._prepare_target_info(versions)
+
+    def _prepare_target_info(self, versions: dict):
+        deps = self.depender.get_deps_list(self.name)
+        for dep in deps:
+            t = TargetInfo(name=dep, install_path=self.install_path,
+                           version=versions.get(dep, global_info[dep]["version"][0]),
+                           force_build=False)
+            self.targets_list.append(t)
+            self.targets_map[dep] = t
+
+    def update(self, update_deps, proxies):
+        need_update_targets = self.targets_list if update_deps else self.targets_list[-1:]
+
+        self.downloader.downloads(
+            repos=dict((t.name, t.version) for t in need_update_targets), proxies=proxies)
+
+    def build(self, build_deps, force_build):
+        self.targets_map[self.name].force_build = force_build
+        need_build_deps = self.targets_list if build_deps else self.targets_list[-1:]
+
+        self.logger.info("start to build \ndeps[%s]", [t.name for t in need_build_deps])
+
+        for t in need_build_deps:
+            ret = self.builder.build(t.name, self.targets_map)
+            if ret != 0:
+                self.logger.critical("build error with %d", ret)
