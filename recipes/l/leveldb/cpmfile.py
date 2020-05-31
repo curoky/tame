8f040ea3c0088d53047517c480c577c3c456ffdd
curoky <cccuroky@gmail.com>
2020-05-31 23:15:32 +0800

    [cpm]: add recipes

---
+++ recipes/l/leveldb/cpmfile.py
@@ -0,0 +1,33 @@
+# Copyright 2021 curoky(cccuroky@gmail.com).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cpm.recipes import (CmakeOption, CmakeTargetPropertie, GitOption,
+                         add_cmake_recipe)
+
+add_cmake_recipe(
+    name='leveldb',
+    git_options=GitOption(url='https://github.com/google/leveldb',),
+    include_dirs=[
+        '.',
+    ],
+    cmake_options=[
+        CmakeOption(key='LEVELDB_INSTALL', value='ON'),
+        CmakeOption(key='LEVELDB_BUILD_TESTS', value='OFF'),
+        CmakeOption(key='LEVELDB_BUILD_BENCHMARKS', value='OFF'),
+    ],
+    cmake_target_properties=[
+        CmakeTargetPropertie(key='CXX_STANDARD', value='17'),
+    ],
+    link_libraries=['leveldb'],
+)
